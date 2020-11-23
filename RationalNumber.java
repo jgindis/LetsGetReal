d2745f74bd88d328a1e27a54092d7ad812601cad
jgindis <jgindis20@stuy.edu>
2020-11-22 23:18:10 -0500

    Added reciprocal()

--- RationalNumber.java
+++ RationalNumber.java
@@ -33,7 +33,7 @@ public class RationalNumber extends RealNumber {
   *and denominator as this RationalNumber but reversed.
   */
   public RationalNumber reciprocal(){
-    return null;
+    return new RationalNumber(denominator, numerator);
   }
   /**
   *@return true when the RationalNumbers have the same numerators and denominators, false otherwise.
