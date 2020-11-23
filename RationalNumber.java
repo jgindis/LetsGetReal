ab02825f5fe98e60b1f29d866cb9cdd0d1fdcff7
jgindis <jgindis20@stuy.edu>
2020-11-22 23:20:05 -0500

    Added equals()

--- RationalNumber.java
+++ RationalNumber.java
@@ -39,7 +39,8 @@ public class RationalNumber extends RealNumber {
   *@return true when the RationalNumbers have the same numerators and denominators, false otherwise.
   */
   public boolean equals(RationalNumber other){
-    return false;
+    return (numerator == other.getNumerator() &&
+            denominator == other.getDenominator());
   }
 
   /**
