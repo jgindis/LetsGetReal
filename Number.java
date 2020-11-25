751c80896c085cce99b6dbf661fc32c24f433708
jgindis <jgindis20@stuy.edu>
2020-11-25 13:00:37 -0500

    Added compareTo()

--- Number.java
+++ Number.java
@@ -1,5 +1,15 @@
 public abstract class Number {
 
+  public abstract double getValue();
+
+  public int compareTo(Number other){
+   int var = 0;
+   if (getValue() > other.getValue()) var = 1;
+   if (getValue() == other.getValue()) var = 0;
+   if (getValue() < other.getValue()) var = -1;
+   return var;
+ }
+
   public boolean equals(Number other) {
     return (getValue() == other.getValue());
   }
